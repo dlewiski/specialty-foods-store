553c808b88291f653b9a87b16ad7047dda0e6524
David Lewis <davidlewisp@gmail.com>
2018-04-20 10:55:54 -0700

    write and pass spec to validate review content_body  is between 50 and 250 characters

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review <ActiveRecord::Base
   validates :product_id, :author, :content_body, :rating, :presence => true
 
   validates_inclusion_of :rating, in: 1..5
+  validates_length_of :content_body, in: 50..250
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,5 @@ describe Review do
   it { should validate_presence_of :rating }
 
   it { should validate_inclusion_of(:rating).in_range(1..5) }
+  it { should validate_length_of(:content_body).is_at_least(50).is_at_most(250) }
 end
