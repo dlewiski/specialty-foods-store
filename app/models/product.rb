1baef536201fb1e10ccd70fa30d7c8b846997415
David Lewis <davidlewisp@gmail.com>
2018-04-20 12:50:10 -0700

    create method to set every 4th ingredient to be from USA

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
+include ActionView::Helpers::NumberHelper
+
 class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :cost, :origin, :presence => true
+
+  def convertCost
+    self.cost = number_to_currency(self.cost)
+  end
+
+  def setOriginUSA
+    if self.id % 4 == 0
+      self.origin = "USA"
+    else
+      self.origin
+    end
+  end
 end
