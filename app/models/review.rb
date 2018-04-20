c8919561b9a8219fe559d46273c9593cf1cf9704
David Lewis <davidlewisp@gmail.com>
2018-04-20 10:48:44 -0700

    write and pass spec to validate review rating is between 1-5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review <ActiveRecord::Base
+
   belongs_to :product
   validates :product_id, :author, :content_body, :rating, :presence => true
+
+  validates_inclusion_of :rating, in: 1..5
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,5 +6,5 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
 
-  it { should belong_to :product }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
 end
